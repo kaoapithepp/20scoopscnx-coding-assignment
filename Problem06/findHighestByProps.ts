4662543fd98ef506f8c07f5209311026e4fa0821
Apithep P <49969602+kaoapithepp@users.noreply.github.com>
2022-11-09 16:38:03 +0700

    fix: Change .js to .ts

--- Problem06/findHighestByProps.ts
+++ Problem06/findHighestByProps.ts
@@ -1,8 +1,15 @@
-const INPUT_ARRAY = require('../CHALLENGE_B');
+import INPUT_ARRAY from '../CHALLENGE_B';
+
+type UserEntity = {
+    name: string,
+    age: number,
+    salary: number,
+    children: number
+}[];
 
 const PROPERTY = "children";
 
-function findHighestByProps(input, props) {
+function findHighestByProps(input: UserEntity, props: string) {
     var compare = input[0];
     for (var i = 0; i < input.length; i++) {
         if (input[i][props] > compare[props]) compare = input[i]; 
